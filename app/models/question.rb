d3dad86b2f59277fe00e496bf7eeb15f8f15a5ef
motsat <konpeiex@gmail.com>
2018-07-22 13:53:35 +0900

    question validation

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,2 +1,5 @@
 class Question < ApplicationRecord
+  validates :title, presence: true, length: { maximum: 300 }
+  validates :right_answer, presence: true, length: { maximum: 100 }
+  validates :display_order, presence: true, uniqueness: true
 end
