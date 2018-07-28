1f15aedae121397702ad1764e8a6ba958869ef0d
motsat <konpeiex@gmail.com>
2018-07-28 16:19:21 +0900

    add questions system test

---
+++ spec/system/questions_spec.rb
@@ -0,0 +1,61 @@
+require 'rails_helper'
+
+RSpec.describe 'Questions', type: :system do
+  describe "show default quiz" do
+    let!(:question) { FactoryBot.create(:question, title: "How many vowels are there in the English alphabet", right_answer: 47, display_order: 1) }
+
+    before { visit root_path }
+    it 'show first question' do
+      # Question編集画面を開く
+      expect(page).to have_content question.title
+    end
+  end
+
+  describe "can answer" do
+    context "When answer is right" do
+      context "When there is a next question" do
+        let!(:question_1) { FactoryBot.create(:question, title: "How many vowels are there in the English alphabet", right_answer: 47, display_order: 1) }
+        let!(:question_2) { FactoryBot.create(:question, title: "1.0 + 2.1 = ?", right_answer: 3.1, display_order: 2) }
+        before do 
+          visit root_path
+          fill_in "questions_answer", with: 47
+          click_on "Save"
+        end
+
+        it 'show second question and show ok!!' do
+          expect(current_path).to eq question_path(question_2)
+          expect(page).to have_content "ok!!"
+          expect(page).to have_content question_2.title
+        end
+      end
+
+      context "When there is no next question" do
+        let!(:question) { FactoryBot.create(:question, right_answer: 47) }
+        before do 
+          visit root_path
+          fill_in "questions_answer", with: 47
+          click_on "Save"
+        end
+        it 'show Congratulations!!!!!!' do
+          expect(page).to have_content "Congratulations!!!!"
+          expect(current_path).to eq question_answers_path(question)
+        end
+      end
+    end
+
+    context "When answer is wrong" do
+      let!(:question) { FactoryBot.create(:question, title: "1.0 + 2.1 = ?", right_answer: 3.1) }
+      before do 
+        visit root_path
+        fill_in "questions_answer", with: 2
+        click_on "Save"
+      end
+
+      it 'show second question and show ok!!' do
+        expect(current_path).to eq question_path(question)
+        expect(page).to have_content "ng!!"
+        expect(page).to have_content question.title
+      end
+    end
+  end
+end
