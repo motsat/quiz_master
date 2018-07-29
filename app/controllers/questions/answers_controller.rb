2a00e5ad8434d3c8f408d7d4e035b360b5dc3820
motsat <konpeiex@gmail.com>
2018-07-29 22:17:43 +0900

    refactor answer controller

--- app/controllers/questions/answers_controller.rb
+++ app/controllers/questions/answers_controller.rb
@@ -2,21 +2,27 @@ class Questions::AnswersController < ApplicationController
   def create
     question = Question.find params[:question_id]
     answer = question_params[:answer].to_s
-    unless Quiz::Judgementer.new(question, answer).right?
+    if Quiz::Judgementer.new(question, answer).right?
+      return show_completed_or_next_question(question)
+    else
       flash[:answer_wrong] = true
       return redirect_to question_path(question)
     end
-
-    next_question = question.next_question
-    if next_question
-      flash[:answer_right] = true
-      return redirect_to question_path(next_question)
-    else
-      render "completed"
-    end
   end
+
+  private 
 
   def question_params
     params.require(:questions).permit(:answer)
+  end
+
+  def show_completed_or_next_question(current_question)
+    next_question = current_question.next_question
+    if next_question
+      flash[:answer_right] = true
+      redirect_to question_path(next_question)
+    else
+      render "completed"
+    end
   end
 end
